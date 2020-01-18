b3fddcbfcf33f3e9974b0bca097c7ee5193987b4
luciale <lucialecabrerao@gmail.com>
2020-01-17 22:07:44 -0600

    Get

---
+++ Lab00/Controllers/PeliController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab00.Controllers
+{ 
+     
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliController : ControllerBase
+    {
+        List<Pelicula> pel = new List<Pelicula>();
+        Pelicula peli = new Pelicula();
+
+
+        // GET: api/Peli
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string[] res = new string[10];
+            if (pel.Count() >= 10)
+            {
+                int x = pel.Count()-1;
+                for (int y=0; y<10; y++) {
+                    res[y] =pel[x].nombre;
+                    x--;
+                }
+            }
+
+
+            return res;
+        }
+
+        
+
+        // POST: api/Peli
+        [HttpPost]
+        public void Post([FromBody] string nombre, string director, int año)
+        {
+            peli.nombre = nombre;
+            peli.director = director;
+            peli.año = año;
+            pel.Add(peli);
+        }
+
+    }
+}
---
+++ Lab00/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab00
+{
+    public class Pelicula
+    {
+        public string nombre { set; get; }
+        public string director { set; get; }
+        public int año { set; get; }
+
+
+    }
+}
